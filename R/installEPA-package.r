82980c75cec89195205b4ebdf7de176404792536
dkvale <kvaled@gmail.com>
2016-11-30 10:03:57 -0600

    create package document

---
+++ R/installEPA-package.r
@@ -0,0 +1,7 @@
+#' installEPA: Download EPA air models
+#'
+#' Download a list of EPA air models. Options: aermap, aermod, aerscreen, bpip, makemet.
+#'
+#' @name installEPA
+#' @docType package
+NULL
